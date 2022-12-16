69b89b7f777f823b89de7cecf623485082d482c0
Andre Menezes <djdedemenezes@gmail.com>
2022-12-16 11:20:32 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 1. give feedbacks to your developer
+puts "Destroying restaurants..."
+# 2. clean your daabase
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(
+  name: "Le Dindon en Laisse"
+)
+Restaurant.create!(
+  name: "Neuf et Voisins"
+)
+puts "Finished!"
